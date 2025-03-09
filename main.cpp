a9c0aecf76dd489d08870dd20fbf2e073fae3cc2
mly808299 <mly808299@gmail.com>
2025-03-09 07:50:36 +0330

    commit3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-    std::cout << "Hello and welcome!\n";
+    cout << "Hello and welcome!\n";
+    cout << "Hello and welcome\n";
     return 0;
 }
